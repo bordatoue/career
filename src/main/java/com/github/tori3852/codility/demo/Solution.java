423e88155729d60d8ce384cb556fc4c3c6c3dac6
tori3852 <tori3852@gmail.com>
2013-04-01 19:45:42 +0100

    Demo solution – 100/100.

--- src/main/java/com/github/tori3852/codility/demo/Solution.java
+++ src/main/java/com/github/tori3852/codility/demo/Solution.java
@@ -8,26 +8,41 @@ public class Solution {
 			return -1;
 		} else if (A.length == 0) {
 			return -1;
+		} else if (A.length == 1) {
+			return 0;
 		}
 
 		int N = A.length;
 
-		for (int P = 0; P < N; P++) {
+		long[] l2r_sum_array = new long[N];
+		long l2r_sum = 0;
 
-			long left_sum = 0;
-			for (int i = 0; i < P; i++) {
-				left_sum += A[i];
+		long[] r2l_sum_array = new long[N];
+		long r2l_sum = 0;
+
+		for (int i = 0; i < N; i++) {
+
+			l2r_sum += A[i];
+			r2l_sum += A[N - 1 - i];
+
+			l2r_sum_array[i] = l2r_sum;
+			r2l_sum_array[N - 1 - i] = r2l_sum;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (i == 0) {
+				if (0 == r2l_sum_array[i + 1]) {
+					return i;
+				}
+			} else if (i == N - 1) {
+				if (l2r_sum_array[i - 1] == 0) {
+					return i;
+				}
+			} else {
+				if (l2r_sum_array[i - 1] == r2l_sum_array[i + 1]) {
+					return i;
+				}
 			}
-
-			long right_sum = 0;
-			for (int i = P + 1; i < N; i++) {
-				right_sum += A[i];
-			}
-
-			if (left_sum == right_sum) {
-				return P;
-			}
-
 		}
 
 		return -1;
--- src/test/java/com/github/tori3852/codility/demo/SolutionTest.java
+++ src/test/java/com/github/tori3852/codility/demo/SolutionTest.java
@@ -1,16 +1,16 @@
 package com.github.tori3852.codility.demo;
 
+import java.util.Arrays;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
-
-import com.github.tori3852.codility.demo.Solution;
 
 public class SolutionTest {
 
 	@Test
 	public void test_codility_example() {
-		Assert.assertEquals(new Solution().equi(new int[] { -7, 1, 5, 2, -4, 3, 0 }), 3);
+		Assert.assertEquals(3, new Solution().equi(new int[] { -7, 1, 5, 2, -4, 3, 0 }));
 	}
 
 	@Test
@@ -61,5 +61,30 @@ public class SolutionTest {
 		// 0, 1, sum[0..1]=-4294967296, right sum (empty set)=0
 		Assert.assertEquals(-1, new Solution().equi(new int[] { -Integer.MIN_VALUE, -Integer.MIN_VALUE }));
 	}
- 
+
+	@Test
+	public void test_large_long_sequence_of_ones() throws InterruptedException {
+
+		final int TIME_LIMIT = 1_000;
+		final int ARRAY_SIZE = 100_001; // must be odd
+
+		final int[] dest = new int[ARRAY_SIZE];
+		Arrays.fill(dest, 1);
+
+		Thread thread = new Thread() {
+
+			@Override
+			public void run() {
+				Assert.assertEquals(ARRAY_SIZE / 2, new Solution().equi(dest));
+			}
+
+		};
+
+		thread.start();
+
+		thread.join(TIME_LIMIT);
+
+		Assert.assertFalse("Computation took too long.", thread.isAlive());
+	}
+
 }

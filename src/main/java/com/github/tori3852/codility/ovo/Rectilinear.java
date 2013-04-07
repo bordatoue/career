b6b103deba6c3bd9f24a2c0d191c3afc791c461c
tori3852 <tori3852@gmail.com>
2013-04-07 19:52:26 +0100

    OVO online test (rectilinear).

--- src/main/java/com/github/tori3852/codility/ovo/Rectilinear.java
+++ src/main/java/com/github/tori3852/codility/ovo/Rectilinear.java
@@ -2,24 +2,53 @@ package com.github.tori3852.codility.ovo;
 
 public class Rectilinear {
 
-	public int area_of_sum(int K, int L, int M, int N, int P, int Q, int R, int S) {
+	public int area_of_sum(
+	//
+			int X_bottom_left_1, int Y_bottom_left_1,
+			//
+			int X_top_right_1, int Y_top_right_1,
+			//
+			int X_bottom_left_2, int Y_bottom_left_2,
+			//
+			int X_top_right_2, int Y_top_right_2) {
 
-		int area1 = getArea(K, L, M, N);
-		int area2 = getArea(P, Q, R, S);
+		int area1 = getArea(X_bottom_left_1, Y_bottom_left_1, X_top_right_1, Y_top_right_1);
+		int area2 = getArea(X_bottom_left_2, Y_bottom_left_2, X_top_right_2, Y_top_right_2);
 
-		int intersectingArea = getIntercectingArea(K, L, M, N, P, Q, R, S);
-		System.out.println(intersectingArea);
+		int intersectingArea = getIntercectingArea(
+		//
+				X_bottom_left_1, Y_bottom_left_1,
+				//
+				X_top_right_1, Y_top_right_1,
+				//
+				X_bottom_left_2, Y_bottom_left_2,
+				//
+				X_top_right_2, Y_top_right_2);
+
 		return area1 + area2 - intersectingArea;
 	}
 
-	protected int getIntercectingArea(int K, int L, int M, int N, int P, int Q, int R, int S) {
-		int horizontalIntercection = Math.max(0, Math.min(M, R) - Math.max(K, P));
-		int verticalIntersection = Math.max(0, Math.min(N, S) - Math.max(L, Q));
+	/* default */int getIntercectingArea(
+	//
+			int X_bottom_left_1, int Y_bottom_left_1,
+			//
+			int X_top_right_1, int Y_top_right_1,
+			//
+			int X_bottom_left_2, int Y_bottom_left_2,
+			//
+			int X_top_right_2, int Y_top_right_2) {
 
+		int horizontalIntercection = getIntercepctionLength(X_bottom_left_1, X_top_right_1, X_bottom_left_2, X_top_right_2);
+		int verticalIntersection = getIntercepctionLength(Y_bottom_left_1, Y_top_right_1, Y_bottom_left_2, Y_top_right_2);
+		
 		return horizontalIntercection * verticalIntersection;
 	}
 
-	protected int getArea(int x1, int y1, int x2, int y2) {
+	private int getIntercepctionLength(int start1, int end1, int start2, int end2) {
+		return Math.max(0, Math.min(end1, end2) - Math.max(start1, start2));
+	}
+
+	/* default */int getArea(int x1, int y1, int x2, int y2) {
 		return (x2 - x1) * (y2 - y1);
 	}
 
--- src/test/java/com/github/tori3852/codility/ovo/RectilinearTest.java
+++ src/test/java/com/github/tori3852/codility/ovo/RectilinearTest.java
@@ -4,19 +4,107 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
-public class RectilinearTest {
+public final class RectilinearTest {
 
 	@Test
 	public void test_codility_example1() {
 
-		int K = -4, L = 1, M = 2, N = 6, P = 0, Q = -1, R = 4, S = 3;
+		int
+		//
+		X_bottom_left_1 = -4, Y_bottom_left_1 = 1,
+		//
+		X_top_right_1 = 2, Y_top_right_1 = 6,
+		//
+		X_bottom_left_2 = 0, Y_bottom_left_2 = -1,
+		//
+		X_top_right_2 = 4, Y_top_right_2 = 3;
 
-		Assert.assertEquals(42, new Rectilinear().area_of_sum(K, L, M, N, P, Q, R, S));
+		Assert.assertEquals(42, new Rectilinear().area_of_sum(
+		//
+				X_bottom_left_1, Y_bottom_left_1,
+				//
+				X_top_right_1, Y_top_right_1,
+				//
+				X_bottom_left_2, Y_bottom_left_2,
+				//
+				X_top_right_2, Y_top_right_2));
 	}
 
 	@Test
 	public void test_getArea() {
 		Assert.assertEquals(9, new Rectilinear().getArea(-1, -1, 2, 2));
+	}
+
+	@Test
+	public void test_getIntercectingArea_codility_example() {
+
+		int
+		//
+		X_bottom_left_1 = -4, Y_bottom_left_1 = 1,
+		//
+		X_top_right_1 = 2, Y_top_right_1 = 6,
+		//
+		X_bottom_left_2 = 0, Y_bottom_left_2 = -1,
+		//
+		X_top_right_2 = 4, Y_top_right_2 = 3;
+
+		Assert.assertEquals(4, new Rectilinear().getIntercectingArea(
+		//
+				X_bottom_left_1, Y_bottom_left_1,
+				//
+				X_top_right_1, Y_top_right_1,
+				//
+				X_bottom_left_2, Y_bottom_left_2,
+				//
+				X_top_right_2, Y_top_right_2));
+	}
+
+	@Test
+	public void test_getIntercectingArea_X_not_intercecting() {
+
+		int
+		//
+		X_bottom_left_1 = 0, Y_bottom_left_1 = 0,
+		//
+		X_top_right_1 = 1, Y_top_right_1 = 1,
+		//
+		X_bottom_left_2 = 2, Y_bottom_left_2 = 0,
+		//
+		X_top_right_2 = 3, Y_top_right_2 = 1;
+
+		Assert.assertEquals(0, new Rectilinear().getIntercectingArea(
+		//
+				X_bottom_left_1, Y_bottom_left_1,
+				//
+				X_top_right_1, Y_top_right_1,
+				//
+				X_bottom_left_2, Y_bottom_left_2,
+				//
+				X_top_right_2, Y_top_right_2));
+	}
+
+	@Test
+	public void test_getIntercectingArea_Y_not_intercecting() {
+
+		int
+		//
+		X_bottom_left_1 = 0, Y_bottom_left_1 = 0,
+		//
+		X_top_right_1 = 1, Y_top_right_1 = 1,
+		//
+		X_bottom_left_2 = 0, Y_bottom_left_2 = 2,
+		//
+		X_top_right_2 = 1, Y_top_right_2 = 3;
+
+		Assert.assertEquals(0, new Rectilinear().getIntercectingArea(
+		//
+				X_bottom_left_1, Y_bottom_left_1,
+				//
+				X_top_right_1, Y_top_right_1,
+				//
+				X_bottom_left_2, Y_bottom_left_2,
+				//
+				X_top_right_2, Y_top_right_2));
 	}
 
 }

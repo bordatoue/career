f94ee73050a3a4da8869b98a37aaf211fdfb2fb9
tori3852 <tori3852@gmail.com>
2013-04-07 19:47:43 +0100

    OVO online test (increasing subsequences).

--- src/main/java/com/github/tori3852/codility/ovo/IncreasingSubsequence.java
+++ src/main/java/com/github/tori3852/codility/ovo/IncreasingSubsequence.java
@@ -1,21 +1,72 @@
 package com.github.tori3852.codility.ovo;
 
-public class IncreasingSubsequence {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public final class IncreasingSubsequence {
+
+	private static final int MAGIC_MODULUS = 1_000_000_007;
+
+	// 2 ≤ K ≤ N
+	// is - [P[0], P[1], ..., P[K-1]]
+	// 0 ≤ P[1] < P[2] < ... < P[K] < N (indices increase);
+	// A[P[1]] < A[P[2]] < ... < A[P[K]] (values increase).
+	// returns the remainder modulo 1,000,000,007 of the number of different
+	// increasing subsequences of array A.
 	public int count_incr_subseqs(int[] A) {
 
-		// 2 ≤ K ≤ N
-		// is - [P[0], P[1], ..., P[K-1]]
-		// 0 ≤ P[1] < P[2] < ... < P[K] < N (indices increase);
-		// A[P[1]] < A[P[2]] < ... < A[P[K]] (values increase).
-		// ... write your code here ...
+		int N = getSequences(getList(A)).size();
 
-		// returns the remainder modulo 1,000,000,007 of the number of different
-		// increasing subsequences of array A.
+		return N % MAGIC_MODULUS;
+	}
 
-		int N = 9;
+	private List<Integer> getList(int[] A) {
+		List<Integer> list = new ArrayList<>(A.length);
+		for (int i : A) {
+			list.add(i);
+		}
+		return list;
+	}
 
-		return N % 1_000_000_007;
+	/* default */List<List<Integer>> getSequences(List<Integer> availableSequence) {
+		return getSequences(Collections.<Integer> emptyList(), availableSequence);
+	}
+
+	/* default */List<List<Integer>> getSequences(List<Integer> currentSequence, List<Integer> availableSequence) {
+
+		List<List<Integer>> result = new ArrayList<>();
+
+		if (isValidSequence(currentSequence)) {
+			result.add(currentSequence);
+		}
+
+		for (int i = 0; i < availableSequence.size(); i++) {
+
+			Integer element = availableSequence.get(i);
+
+			if (canAddElementToSequence(currentSequence, element)) {
+
+				List<Integer> newCurrentSequence = new ArrayList<>(currentSequence);
+				newCurrentSequence.add(element);
+
+				List<Integer> newAvailableSequence = availableSequence.subList(i + 1, availableSequence.size());
+
+				result.addAll(getSequences(newCurrentSequence, newAvailableSequence));
+
+			}
+
+		}
+
+		return result;
+	}
+
+	/* default */boolean isValidSequence(final List<Integer> currentSequence) {
+		return currentSequence.size() > 1;
+	}
+
+	/* default */boolean canAddElementToSequence(final List<Integer> currentSequence, final Integer element) {
+		return currentSequence.size() == 0 || currentSequence.get(currentSequence.size() - 1) < element;
 	}
 
 }
--- src/test/java/com/github/tori3852/codility/ovo/IncreasingSubsequenceTest.java
+++ src/test/java/com/github/tori3852/codility/ovo/IncreasingSubsequenceTest.java
@@ -1,14 +1,107 @@
 package com.github.tori3852.codility.ovo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
 
-public class IncreasingSubsequenceTest {
+public final class IncreasingSubsequenceTest {
+
+	@Test
+	public void test_isValidSequence_empty_list() {
+		Assert.assertFalse(new IncreasingSubsequence().isValidSequence(Collections.<Integer> emptyList()));
+	}
+
+	@Test
+	public void test_isValidSequence_too_small_list() {
+		Assert.assertFalse(new IncreasingSubsequence().isValidSequence(Arrays.asList(1)));
+	}
+
+	@Test
+	public void test_isValidSequence_ok() {
+		Assert.assertTrue(new IncreasingSubsequence().isValidSequence(Arrays.asList(1, 2)));
+	}
+
+	@Test
+	public void test_canAddElementToSequence_empty_list() {
+		Assert.assertTrue(new IncreasingSubsequence().canAddElementToSequence(Collections.<Integer> emptyList(), 1));
+	}
+
+	@Test
+	public void test_canAddElementToSequence_smaller() {
+		Assert.assertFalse(new IncreasingSubsequence().canAddElementToSequence(Arrays.asList(1, 2), 1));
+
+	}
+
+	@Test
+	public void test_canAddElementToSequence_equal() {
+		Assert.assertFalse(new IncreasingSubsequence().canAddElementToSequence(Arrays.asList(1, 2), 2));
+	}
+
+	@Test
+	public void test_canAddElementToSequence_bigger() {
+		Assert.assertTrue(new IncreasingSubsequence().canAddElementToSequence(Arrays.asList(1, 2), 3));
+	}
+
+	private List<List<Integer>> toListOfLists(Integer[][] expectedArray) {
+		List<List<Integer>> expected = new ArrayList<>();
+		for (Integer[] list : expectedArray) {
+			expected.add(Arrays.asList(list));
+		}
+		return expected;
+	}
+
+	@Test
+	public void test_getSequences_simple_12() {
+
+		List<Integer> sequence = Arrays.asList(1, 2);
+		List<List<Integer>> expected = toListOfLists(new Integer[][] { { 1, 2 } });
+
+		List<List<Integer>> result = new IncreasingSubsequence().getSequences(sequence);
+
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void test_getSequences_simple_123() {
+
+		List<Integer> sequence = Arrays.asList(1, 2, 3);
+		List<List<Integer>> expected = toListOfLists(new Integer[][] { { 1, 2 }, { 1, 2, 3 }, { 1, 3 }, { 2, 3 } });
+
+		List<List<Integer>> result = new IncreasingSubsequence().getSequences(sequence);
+
+		Assert.assertEquals(expected, result);
+
+	}
+
+	@Test
+	public void test_getSequences_codility() {
+
+		List<Integer> sequence = Arrays.asList(4, 6, 7, 3, 2, 7);
+		List<List<Integer>> expected = toListOfLists(new Integer[][] { { 4, 6 }, { 4, 6, 7 }, { 4, 6, 7 }, { 4, 7 }, { 4, 7 }, { 6, 7 }, { 6, 7 }, { 3, 7 },
+				{ 2, 7 } });
+
+		List<List<Integer>> result = new IncreasingSubsequence().getSequences(sequence);
+
+		Assert.assertEquals(expected, result);
+
+	}
+
+	@Test
+	public void test_simple_123() {
+
+		int[] test = new int[] { 1, 2, 3 };
+
+		Assert.assertEquals(4, new IncreasingSubsequence().count_incr_subseqs(test));
+	}
 
 	@Test
 	public void test_codility_example1() {
-		
+
 		int[] test = new int[] { 4, 6, 7, 3, 2, 7 };
 
 		Assert.assertEquals(9, new IncreasingSubsequence().count_incr_subseqs(test));
